c43e20a34003be7c86487e75a1942636f31d1842
Willian Menezes dos Santos <willian.santos@bs2tecnologia.com.br>
2021-12-08 18:13:39 -0300

    Alterado configuracao de index depreciada

--- Learn-Entity-Core/Data/Configurations/ClienteConfig.cs
+++ Learn-Entity-Core/Data/Configurations/ClienteConfig.cs
@@ -15,7 +15,7 @@ namespace Learn_Entity_Core.Data.Configurations
             builder.Property(p => p.CEP).HasColumnType("CHAR(8)").IsRequired();
             builder.Property(p => p.Estado).HasColumnType("CHAR(2)").IsRequired();
             builder.Property(p => p.Cidade).HasMaxLength(60).IsRequired();
-            builder.HasIndex(i => i.Telefone).HasName("idx_cliente_telefone");
+            builder.HasIndex(i => i.Telefone).HasDatabaseName("idx_cliente_telefone");
         }
     }
 }
