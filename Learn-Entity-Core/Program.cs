f5141e2650876cf18a0c0c9a814f713f06522b87
Willian Menezes dos Santos <willian.santos@bs2tecnologia.com.br>
2021-12-08 16:16:27 -0300

    first commit

---
+++ Learn-Entity-Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Learn_Entity_Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
