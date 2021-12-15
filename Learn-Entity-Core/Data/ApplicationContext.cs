9bdbdaa21ea79e25e94f7c98f9e11af5ba3e7011
Willian Menezes dos Santos <willian.santos@bs2tecnologia.com.br>
2021-12-15 12:41:28 -0300

    Inserindo dados

--- Learn-Entity-Core/Data/ApplicationContext.cs
+++ Learn-Entity-Core/Data/ApplicationContext.cs
@@ -6,6 +6,8 @@ namespace Learn_Entity_Core.Data
     public class ApplicationContext : DbContext
     {
         public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
--- Learn-Entity-Core/Program.cs
+++ Learn-Entity-Core/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Learn_Entity_Core.Data;
+using Learn_Entity_Core.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace Learn_Entity_Core
 {
@@ -6,7 +9,35 @@ namespace Learn_Entity_Core
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            InserirDados();
+        }
+
+        private static void InserirDados()
+        {
+            var produto = new Produto
+            {
+                Ativo = true,
+                CodigoDeBarras = "123",
+                Descricao = "Produto de testes",
+                TipoProduto = ValueObjects.TipoProduto.MercadoriaParaRevenda,
+                Valor = 100
+            };
+
+            // adicionando pela propriedade que mapeamos no contexto
+            var db = new ApplicationContext();
+            db.Produtos.Add(produto);
+
+            //adicionando chamando o DbSet
+            db.Set<Produto>().Add(produto);
+
+            // forcando o mapeamento no entry
+            db.Entry(produto).State = EntityState.Added;
+
+            //adicionar pelo db
+            db.Add(produto);
+
+            //salvando a entidade no banco de dados, antes so estava mapeados.
+            db.SaveChanges();
         }
     }
 }
